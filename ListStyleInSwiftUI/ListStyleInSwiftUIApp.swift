fb57f8c7a2e60626a6b39ce1f59fdb7037e0fc83
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-15 10:21:45 +0200

    Initial Commit

---
+++ ListStyleInSwiftUI/ListStyleInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListStyleInSwiftUIApp.swift
+//  ListStyleInSwiftUI
+//
+//  Created by ramil on 15.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ListStyleInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
